4193517cc84f770dbc3dc170b65f89f05e3f76c6
Isaac Fonseca <isaacfs74@gmail.com>
2021-07-02 05:41:45 -0600

    FIFO completado. Las pruebas están en el directorio testbench

--- parte2_capa_transaccion/FIFO/rtl/FIFO.v
+++ parte2_capa_transaccion/FIFO/rtl/FIFO.v
@@ -1,60 +1,116 @@
-`include "memoria.v"
-module FIFO (
+`include "rtl/memoria.v"
+module FIFO  #(
+    parameter ALMOST_EMPTY_THRESHOLD = 2,
+    parameter ALMOST_FULL_THRESHOLD = 6,
+    parameter FIFO_DEPTH = 8, // DEBE SER UNA POTENCIA DE 2
+    parameter FIFO_WORD_SIZE = 10,
+    parameter FIFO_PTR_SIZE = $clog2(FIFO_DEPTH)
+) (
     input clk,
     input reset_L,
-    input [9:0] data_in,
+    input [FIFO_WORD_SIZE-1:0] data_in,
     input wr_en, // En arquitectura fifo_wr
     input rd_en, // En arquitectura fifo_rd
-    output reg [9:0] data_out,
-    output reg empty,
-    output reg full,
-    output reg almost_empty,
-    output reg almost_full
-);
+    output [FIFO_WORD_SIZE-1:0] data_out,
+    output reg empty_flag,
+    output reg full_flag,
+    output reg almost_empty_flag,
+    output reg almost_full_flag,
+    output reg error_flag);
     
 // Un push es hacer un enable de write y poner el dato
 // Un pop es hacer un enable de read y sacar el dato
 
 // Memoria del FIFO
-reg [2:0] rd_ptr, wr_ptr;
+reg [FIFO_PTR_SIZE-1:0] rd_ptr, wr_ptr;
 
-memoria memoria_fifo(
-		     // Outputs
-		     .data_out		(data_out[9:0]),
-		     // Inputs
-		     .clk		(clk),
-		     .wr_en		(wr_en),
-		     .rd_en		(rd_en),
-		     .reset_L		(reset_L),
-		     .data_in		(data_in[9:0]),
-		     .wr_ptr		(wr_ptr[2:0]),
-		     .rd_ptr		(rd_ptr[2:0]));
+memoria #(
+    .MEM_DEPTH (FIFO_DEPTH),
+    .WORD_SIZE (FIFO_WORD_SIZE),
+    .PTR_SIZE (FIFO_PTR_SIZE) // $clog2() calcular el logaritmo en base 2
+) memoria_fifo (
+    // Outputs
+    .data_out	(data_out[FIFO_WORD_SIZE-1:0]),
+    // Inputs
+    .clk		(clk),
+    .wr_en		(wr_en),
+    .rd_en		(rd_en),
+    .reset_L	(reset_L),
+    .data_in	(data_in[FIFO_WORD_SIZE-1:0]),
+    .wr_ptr		(wr_ptr[FIFO_PTR_SIZE-1:0]),
+    .rd_ptr		(rd_ptr[FIFO_PTR_SIZE-1:0]) 
+);
 
-// Write logic -> IN: fifo_wr OUT: wr_ptr fifo_flag_wr
+// Write logic -> IN: fifo_wr OUT: wr_ptr wr_ptr_to_control
 always @(posedge clk) begin
-    if(~reset_L) begin
+    if(~reset_L)
         wr_ptr <= 0;
-    end
-    else begin
-        if (wr_en):
-            wr_ptr <= wr_ptr + 1;
-    end
+    else if (wr_en)
+        wr_ptr <= wr_ptr + 1; // wr_ptr_to_control         
 end
 
-// Read logic -> IN: fifo_rd OUT: rd_ptr fifo_flag_rd
+// Read logic -> IN: fifo_rd OUT: rd_ptr rd_ptr_to_control
 always @(posedge clk) begin
-    if(~reset_L) begin
+    if(~reset_L)
         rd_ptr <= 0;
-    end
-    else begin
-        if (rd_en):
-            rd_ptr <= rd_ptr + 1;
-    end
+    else if (rd_en)
+        rd_ptr <= rd_ptr + 1; // rd_ptr_to_control    
 end
 
-// FIFO flag control logic -> IN: fifo_flag_wr fifo_flag_rd 
-//                         -> OUT: fifo_empty fifo_full 
-//                                 fifo_almost_full 
-//                                 fifo_almost_empty
+// FIFO flag control logic -> IN: wr_ptr_to_control rd_ptr_to_control
+//                         -> OUT: empty_flag
+//                                 full_flag
+//                                 almost_full_flag
+//                                 almost_empty_flag
+//                                 error_flag
+
+reg [FIFO_PTR_SIZE-1:0] N, ff_N;
+
+always @(*) begin
+    N = wr_ptr - rd_ptr; // Donde N es el número de elementos en memoria
+end
+
+always @(posedge clk) begin
+    if (~reset_L) begin
+        ff_N <= 0;
+        empty_flag <= 1; 
+        full_flag <= 0;
+        almost_full_flag <= 0;
+        almost_empty_flag <= 0;
+        error_flag <= 0;
+    end
+    else begin
+        ff_N <= N;
+
+        if (N > 0 && N < FIFO_DEPTH) begin // Si tiene datos
+
+            if(N <= ALMOST_EMPTY_THRESHOLD)
+                almost_empty_flag <= 1;
+            else 
+                almost_empty_flag <= 0;
+
+            if(N >= ALMOST_FULL_THRESHOLD && !(ff_N == FIFO_DEPTH-1))
+                almost_full_flag <= 1;
+            else    
+                almost_full_flag <= 0;
+        end   
+
+        if (N != 0) begin
+            empty_flag <= 0;
+            full_flag <= 0;
+        end
+        else if (N == 0) begin
+            if(!(ff_N == FIFO_DEPTH-1))
+                empty_flag <= 1;
+            else
+                empty_flag <= 0;
+
+            if((ff_N == FIFO_DEPTH-1))
+                full_flag <= 1;
+            else    
+                full_flag <= 0;
+        end
+    end
+end
 
 endmodule
---
+++ parte2_capa_transaccion/FIFO/testbench/probador.v
@@ -0,0 +1,98 @@
+module probador #(
+    parameter ALMOST_EMPTY_THRESHOLD = 2,
+    parameter ALMOST_FULL_THRESHOLD = 6,
+    parameter FIFO_DEPTH = 8, // DEBE SER UNA POTENCIA DE 2
+    parameter FIFO_WORD_SIZE = 10,
+    parameter FIFO_PTR_SIZE = $clog2(FIFO_DEPTH)
+) ( 
+	// Módulo probador: generador de señales y monitor de datos.
+	// Entradas del monitor de datos
+		// Condicional
+    input [FIFO_WORD_SIZE-1:0] data_out,
+    input empty_flag,
+    input full_flag,
+    input almost_empty_flag,
+    input almost_full_flag,
+    input error_flag,
+		// Síntesis
+	input [FIFO_WORD_SIZE-1:0] data_out_SYNTH,
+    input empty_flag_SYNTH,
+    input full_flag_SYNTH,
+    input almost_empty_flag_SYNTH,
+    input almost_full_flag_SYNTH,
+    input error_flag_SYNTH,
+    // Salidas del generador de señales
+    output reg clk,
+    output reg reset_L,
+    output reg [FIFO_WORD_SIZE-1:0] data_in,
+    output reg wr_en, // En arquitectura fifo_wr
+    output reg rd_en); // En arquitectura fifo_rd
+	
+
+    integer prob_i; // contador
+
+	// checker
+    integer check = 1;
+    always @(*) begin
+		check = 0;
+        if (data_out == data_out_SYNTH) 
+            check = 1;    
+        else
+            check = 0;    
+    end 
+
+    // Reloj
+	initial	clk <= 0;			// Valor inicial al reloj, sino siempre será indeterminado
+	always #10 clk <= ~clk;		// Hace "toggle" cada 2*1ns
+
+	// Bloque de procedimiento, no sintetizable, se recorre una única vez.
+	// Generalmente, los initial sólo se usan en los testbench o probadores.
+	// Se recomienda siempre sincronizar con el reloj y utilizar asignaciones no bloqueantes en la generación de señales.
+	initial begin
+		$dumpfile("testbench/simulacion.vcd");	// Nombre de archivo del "dump"
+		$dumpvars;              // Directiva para "dumpear" variables
+		// Mensaje que se imprime en consola una vez
+		$display ("\t\ttime\tclk,\tcheck");
+		// Mensaje que se imprime en consola cada vez que un elemento de la lista cambia
+		$monitor($time,"\t%b%d", clk, check);
+        
+        // Inicialización de datos
+		reset_L = 0;
+		wr_en = 0;
+		rd_en = 0;
+		data_in = 10'h000;
+		// Inicio de pruebas
+		
+		@(posedge clk);
+			reset_L <= 1;	
+		
+		@(posedge clk) begin
+			wr_en <= 1;
+			data_in <= 'h001;
+		end
+		
+		repeat (7) begin
+			@(posedge clk) begin
+				wr_en <= 1;
+				data_in <= data_in + 1;
+			end
+		end
+
+		repeat (8) begin
+			@(posedge clk) begin
+				wr_en <= 0; 
+				rd_en <= 1;
+				data_in <= data_in + 1;
+			end
+		end
+
+		@(posedge clk) begin
+			reset_L <= 0;
+		end
+
+		#50
+
+		// Final de pruebas
+		$finish; // Termina de almacenar señales
+	end
+endmodule
