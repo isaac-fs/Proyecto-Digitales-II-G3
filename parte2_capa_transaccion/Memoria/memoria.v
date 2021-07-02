4f9618fc6509ef46b74864cbd51000b73a1c7b89
Isaac Fonseca <isaacfs74@gmail.com>
2021-07-01 19:01:39 -0600

    Parametrización de la memoria

--- parte2_capa_transaccion/Memoria/memoria.v
+++ parte2_capa_transaccion/Memoria/memoria.v
@@ -1,24 +1,27 @@
-module memoria (input clk,
+module memoria #(
+	parameter MEM_DEPTH = 8,
+	parameter WORD_SIZE = 10
+)(input clk,
 	input wr_en,
 	input rd_en,
 	input reset_L,
-	input [9:0] data_in,
+	input [WORD_SIZE-1:0] data_in,
 	input [2:0] wr_ptr, 
 	input [2:0] rd_ptr,
-	output reg [9:0] data_out);
+	output reg [WORD_SIZE-1:0] data_out);
 
    // wr_en y rd_en permiten escritura y lectura respectivamente
    // wr_ptr y rd_ptr brindan las direcciones para lecura y escritura en la memoria
 
    // Memoria de 10 bits de ancho y 8 de profundidad
-   reg [9:0] mem [7:0];
+   reg [WORD_SIZE-1:0] mem [MEM_DEPTH-1:0];
    integer i;
 
    always @(posedge clk)
      begin
 	if(!reset_L) //El reset en bajo limpia la memoria
 	  begin
-	    for (i=0; i<8; i = i+1)
+	    for (i=0; i<MEM_DEPTH; i = i+1)
 			mem[i] <= 0;
 	    	data_out <= 0;
 		end
